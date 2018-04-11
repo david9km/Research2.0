1f03aa911b20f3e2af96dc85d9fef134ed517dca
20180123 <david9km@login01.cluster.cs.jmu.edu>
2018-04-11 15:20:04 -0400

    test 1

---
+++ printArray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* Test 1 */
+/* David9km */
+
+int main (void)
+{
+	int i = 0;
+	int a[] = {7,2,9,18,54,79,101,8,9,21,34,66,1,0,102,19};
+	for(i = 0; i < 16; i++)
+	{
+		printf("%d  ", a[i]);
+	}
+	printf("\n");
+	return 0;
+
+};
